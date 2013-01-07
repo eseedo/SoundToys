0b87fac0385621f94d93e39d110e83284d5b81c7
eseedo <eseedo@gmail.com>
2013-01-07 09:36:00 +0800

    Initial Commit

---
+++ PocketBabysitter/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  PocketBabysitter
+//
+//  Created by Ricky on 1/7/13.
+//  Copyright happybubsy 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ PocketBabysitter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PocketBabysitter
+//
+//  Created by Ricky on 1/7/13.
+//  Copyright happybubsy 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
